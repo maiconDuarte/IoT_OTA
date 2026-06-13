1c65c7896a14483fb4f49a711ae1823cda2342fb
maiconDuarte <73088448+maiconDuarte@users.noreply.github.com>
2026-06-13 19:41:51 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,11 @@
 SSID = "wifi"
 PASSWORD = "senha123"
-pass
+
+AIO_CLIENT = b"ESP32"
+AIO_SERVER = "io.adafruit.com"
+AIO_PORT = 1883
+AIO_USERNAME = "imaicon"
+AIO_KEY = "aio_lTau499apXchlTbEiXrK8mc5h6iT"
+
+aceleracao = b"imaicon/feeds/aceleracao"
+ligaLed = b"imaicon/feeds/ligaLed"
