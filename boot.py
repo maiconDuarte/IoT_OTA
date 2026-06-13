7742f639ae09191501fa64946394aa52de607f7e
maiconDuarte <73088448+maiconDuarte@users.noreply.github.com>
2026-06-13 19:47:26 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,10 +1,7 @@
 from ota import OTAUpdater
-from secrets import SSID, PASSWORD
+import secrets
 
 firmware_url = "https://github.com/maiconDuarte/IoT_OTA/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "boot.py")
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "secrets.py")
+ota_updater = OTAUpdater(secrets.SSID, secrets.PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
-
