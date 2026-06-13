f76c94ecc06e39cb4f1f68b302390840a1da49ec
maiconDuarte <73088448+maiconDuarte@users.noreply.github.com>
2026-06-13 18:32:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from ota import OTAUpdater
+from secrets import SSID, PASSWORD
 
+firmware_url = "https://github.com/maiconDuarte/IoT_OTA/main"
+ota_updater = OTAUpdater(secrets.SSID, secrets.PASSWORD, firmware_url, "iot_main.py")
+ota_updater.download_and_install_update_if_available()
